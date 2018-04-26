6b304ad6f6501c19ee3f0f9fc23d911f1745c789
Nicolas Beck <nicolasbeck.dev@gmail.com>
2018-04-26 14:04:44 +0200

    addiing first rest api

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+
+class UserController extends Controller
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Create User.
+     * @Rest\Post("/api/users", name="user_create")
+     * @param Request $request
+     * @return \FOS\RestBundle\View\View
+     */
+    public function create(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return View::create($user, Response::HTTP_CREATED);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('token', null, ['required'=>false])
+            ->add('name')
+            ->add('email')
+            ->add('password')
+        ;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getBlockPrefix()
+    {
+        return null;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
